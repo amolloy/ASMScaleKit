8c4b15d08ec8fb971657521f745e2499b5165181
Andrew Molloy <amolloy@gmail.com>
2014-07-15 07:55:41 -0400

    Add a test for the HMAC-SHA1 signature for the OAuth1 client.

---
+++ Example/Tests/ASMOAuth1ClientTestsSpec.m
@@ -0,0 +1,39 @@
+//
+//  ASMOAuth1ClientTestsSpec.m
+//  ASMScaleKit
+//
+//  Created by Andrew Molloy on 7/14/14.
+//  Copyright 2014 Andrew Molloy. All rights reserved.
+//
+
+#import <Expecta/Expecta.h>
+#import <ASMSCaleKit/ASMOAuth1Client.h>
+
+SpecBegin(ASMOAuth1Client)
+
+describe(@"ASMOAuth1Client", ^{
+	it(@"should calculate HMAC-SHA1 correctly",
+	   ^{
+		   NSURL* url = [NSURL URLWithString:@"https://oauth.withings.com"];
+		   ASMOAuth1Client* testClient = [[ASMOAuth1Client alloc] initWithBaseURL:url
+																			  key:@"abcdef0123456789abcdef"
+																		   secret:@"abcdef0123456789abcdef"];
+
+		   NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[url URLByAppendingPathComponent:@"account/request_token"]];
+
+		   NSString* signature =
+		   [testClient HMACSHA1SignatureForURLRequest:request
+									  queryParameters:@[@"oauth_callback=http%3A%2F%2Fexample.com%2Fget_access_token",
+														@"oauth_consumer_key=abcdef0123456789abcdef",
+														@"oauth_nonce=f71972b1fa93b8935ccaf34ee02d7657",
+														@"oauth_signature_method=HMAC-SHA1",
+														@"oauth_timestamp=1311778988",
+														@"oauth_version=1.0"]
+								   postBodyParameters:nil
+												token:nil];
+		   expect(signature).to.equal(@"cFj/h2sytYG/7N/szT4OjjdPgD0=");
+	   }
+	   );
+});
+
+SpecEnd
