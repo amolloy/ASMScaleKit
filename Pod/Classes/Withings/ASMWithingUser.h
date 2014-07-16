b46151937db87099bcff01c4b2b041391266a4de
Andrew Molloy <amolloy@gmail.com>
2014-07-16 16:35:38 -0400

    Start fleshing out ASMWithingUser

--- Pod/Classes/Withings/ASMWithingUser.h
+++ Pod/Classes/Withings/ASMWithingUser.h
@@ -9,6 +9,12 @@
 #import <Foundation/Foundation.h>
 #import <ASMScaleKit/ASMScaleUser.h>
 
+@class ASMOAuth1Token;
+
 @interface ASMWithingUser : NSObject <ASMScaleUser>
-@property (nonatomic, assign) NSInteger userid;
+@property (nonatomic, assign, readonly) NSInteger userid;
+@property (nonatomic, strong, readonly) ASMOAuth1Token* accessToken;
+@property (nonatomic, copy, readonly) NSString* name;
+
+
 @end
